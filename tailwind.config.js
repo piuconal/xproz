5288a41e18a8a7a1f13c4c76dd65ff50a304dcb6
Vũ Trọng Hiếu <hcboy1106@gmail.com>
2023-12-20 19:11:57 +0700

    xproz
    14h30-16h30

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'poppins': ['sans-serif', 'Poppins']
+    }
+  },
+  plugins: [],
+}
+
